0fc59813ace3f7109b20f3a73028eaed06603025
Sennaa <senna_vaniersel@hotmail.com>
2018-04-13 17:38:23 +0200

    Normalize input

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,6 +4,8 @@ Logistic Regression from scratch
 Author: Senna van Iersel
 """
 
+
+
 import numpy as np
 
 # Sigmoid function to output an array with values between 0 and 1
@@ -13,7 +15,7 @@ import numpy as np
 #                       b  = (1,1)
 # Output: sigmoid(Z) = (Nx,1)
 def sigmoid(Z):
-    return 1 / (1 + np.exp(-Z))
+    return 1 / (1 + np.e ** (-1*Z))
 
 # Calculate the loss
 # Inputs    y     = True label (0/1)
@@ -38,22 +40,26 @@ def gradient_descent(w, b, alpha, dw, db):
     b = b - alpha * db
     return w, b
 
-def initialization(X):
-    # Row length of X
-    w_len = X.shape[0]
+def initialization(w_len):
     # Initialize weights, bias and cost
     w = dw = np.zeros((w_len,1))
     b = db = cost = 0
     return w, b, cost, dw, db
 
+def normalize(X):
+    mean = np.mean(X, axis=0)
+    sd   = np.std(X)
+    return (X - mean) / sd
+
 # Train the actual logistic regression model
-def LogReg(X, Y, alpha = 0.5, num_iterations = 10000):
+def LogReg(X, Y, alpha = 0.1, num_iterations = 30000):
     X = X.T
+    X = normalize(X)
     Y = Y.reshape(1, X.shape[1])
     # Initialize weights, bias and cost
-    w, b, J, dw, db = initialization(X)
+    w, b, J, dw, db = initialization(X.shape[0])
     m               = X.shape[1]
-    for i in range(1,num_iterations):
+    for i in range(1,num_iterations+1):
         A   = sigmoid(np.dot(w.T,X) + b)
         J   = cost(Y, A, m)
         if i % 1000 == 0:
@@ -63,5 +69,3 @@ def LogReg(X, Y, alpha = 0.5, num_iterations = 10000):
         db  = (1 / m) * np.sum(dZ)
         w,b = gradient_descent(w,b,alpha,dw,db)
     print(J)
-    
-    
